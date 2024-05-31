20aea64d86781f3d10c2154d759d5ae9c8bd979c
cesarAncco <canccor@gmail.com>
2024-05-31 15:47:40 -0500

    Commit: Formulario con los campos basicos segun la guia.

---
+++ js/formulario_validacion.js
@@ -0,0 +1,71 @@
+// document.getElementById("miFormulario").addEventListener("submit", validar_campos)
+
+// function validar_campos () {
+
+// }
+
+const formulario = document.getElementById("formulario");
+const inputs = document.querySelectorAll('#formulario input');
+
+const expresiones = {
+  inputUsuario: /^[a-zA-Z0-9_]{4,16}$/,
+  inputNombre: /^[A-Za-z\s]{4,50}$/,
+  inputContrasena: /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[\W_])[A-Za-z\d\W_]{8,50}$/,
+  //inputRepetirContrasena: /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[\W_])[A-Za-z\d\W_]{8,50}$/,
+};
+
+const campos = {
+  inputUsuario: false,
+  inputNombre: false,
+  inputContrasena: false,
+  inputRepetirContrasena: false,
+};
+
+const validarFormulario = (e) => {
+  e.preventDefault();
+  switch (e.target.name) {
+    case "inputUsuario":
+      validarCampo(expresiones.inputUsuario, e.target, 'inputUsuario');
+      break;
+    case "inputNombre":
+      validarCampo(expresiones.inputNombre, e.target, 'inputNombre');
+      break;
+    case "inputContrasena":
+      validarCampo(expresiones.inputContrasena, e.target, 'inputContrasena');
+      break;
+    case "inputRepetirContrasena":
+      validarContrasena(e.target, 'inputRepetirContrasena');
+      break;
+  }
+
+}
+
+const validarCampo = (expresion, input, campo) => {
+  if(expresion.test(input.value)) {
+    document.getElementById(`${campo}`).classList.add('is-valid');
+    document.getElementById(`${campo}`).classList.remove('is-invalid');
+    campos[campo] = true;
+  } else{
+    document.getElementById(`${campo}`).classList.add('is-invalid');
+    document.getElementById(`${campo}`).classList.remove('is-ivalid');
+    campos[campo] = false;
+  }
+}
+
+const validarContrasena = (input, campo) => {
+  let inputContrasena = document.getElementById("inputContrasena");
+  if (input.value === inputContrasena.value && isNaN(input.value)) {
+    document.getElementById(`${campo}`).classList.add('is-valid');
+    document.getElementById(`${campo}`).classList.remove('is-invalid');
+    campos[campo] = true;
+  } else {
+    document.getElementById(`${campo}`).classList.add('is-invalid');
+    document.getElementById(`${campo}`).classList.remove('is-ivalid');
+    campos[campo] = false;
+  }
+}
+
+inputs.forEach((input) => {
+  // input.addEventListener('keyup', validarFormulario);
+  input.addEventListener('blur', validarFormulario)
+});
