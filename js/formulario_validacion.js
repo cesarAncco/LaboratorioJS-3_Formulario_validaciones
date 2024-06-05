eb3e659e194e226f15f4a88448a98121dd6353f0
cesarAncco <canccor@gmail.com>
2024-06-04 20:55:40 -0500

    Commit Agregando mas validaciones

--- js/formulario_validacion.js
+++ js/formulario_validacion.js
@@ -6,6 +6,9 @@
 
 const formulario = document.getElementById("formulario");
 const inputs = document.querySelectorAll('#formulario input');
+const textarea = document.querySelectorAll('#formulario textarea');
+const selects = document.querySelectorAll('#formulario select');
+const radios = document.querySelectorAll('input[name="inputGenero"]');
 
 const expresiones = {
   inputUsuario: /^[a-zA-Z0-9_]{4,16}$/,
@@ -14,7 +17,8 @@ const expresiones = {
   //inputRepetirContrasena: /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[\W_])[A-Za-z\d\W_]{8,50}$/,
   inputCorreo:  /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
   inputCelular: /^[+51]+[0-9]{9}$/,
-  inputRuc: /[0-9]{11}$/
+  inputRuc: /[0-9]{11}$/,
+  inputComentario: /^[a-zA-Z0-9]{1,100}$/
 };
 
 const campos = {
@@ -24,7 +28,8 @@ const campos = {
   inputRepetirContrasena: false,
   inputCorreo: false,
   inputCelular: false,
-  inputRuc: false
+  inputRuc: false,
+  inputComentario: false
 };
 
 const validarFormulario = (e) => {
@@ -50,6 +55,27 @@ const validarFormulario = (e) => {
       break;
     case "inputRuc":
       validarCampo(expresiones.inputRuc, e.target, 'inputRuc');
+      break;
+    case "inputComentario":
+      validarCampo(expresiones.inputComentario, e.target, 'inputComentario');
+      break;
+    case "inputCuidad":
+      if(e.target.value !==''){
+        document.getElementById('inputCuidad').classList.remove('is-invalid');
+        document.getElementById('inputCuidad').classList.add('is-valid');
+      } else {
+        document.getElementById('inputCuidad').classList.remove('is-valid');
+        document.getElementById('inputCuidad').classList.add('is-invalid');
+      }
+      break;
+    case "inputImagen":
+      if(e.target.value !==''){
+        document.getElementById('inputImagen').classList.remove('is-invalid');
+        document.getElementById('inputImagen').classList.add('is-valid');
+      } else {
+        document.getElementById('inputImagen').classList.remove('is-valid');
+        document.getElementById('inputImagen').classList.add('is-invalid');    
+      }
       break;
   }
 
@@ -83,4 +109,13 @@ const validarContrasena = (input, campo) => {
 inputs.forEach((input) => {
   // input.addEventListener('keyup', validarFormulario);
   input.addEventListener('blur', validarFormulario)
+});
+
+textarea.forEach((input) => {
+  input.addEventListener('blur', validarFormulario)
+});
+
+selects.forEach((select) => {
+  // console.log(select);
+  select.addEventListener('blur' , validarFormulario);
 });
